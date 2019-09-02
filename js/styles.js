bf61352f86c61d4d5b475471f14d71ddf82b6ad8
focused220 <roblhjr.220@gmail.com>
2019-09-02 14:52:55 -0700

    added styles.js

---
+++ js/styles.js
@@ -0,0 +1,15 @@
+import VANTA from './vanta.dots.min.js';
+import * as THREE from './three.r92.min.js'; 
+
+const style = () =>{
+    const page = document.querySelector('body');
+
+    console.log(page);
+}
+
+style(); 
+
+module.exports = style;
+
+
+
