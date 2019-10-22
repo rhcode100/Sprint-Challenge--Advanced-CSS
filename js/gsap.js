0dd0c7d04ce2a4d43709439ff52505ae80580f63
focused220 <roblhjr.220@gmail.com>
2019-10-22 10:49:39 -0700

    Additional styling added to landing page header.

---
+++ js/gsap.js
@@ -0,0 +1,9 @@
+import TweenMax from 'gsap'; 
+
+export const showSignUp = () =>{
+    const walkContent = document.querySelector('.walk-content');
+    const signUp = document.querySelector('.sign-up');
+
+    console.log(walkContent);
+    console.log(sign)
+}
--- js/header.js
+++ js/header.js
@@ -4,11 +4,13 @@ const headerView = () => {
     let canvas = document.querySelector('canvas');
     let context = canvas.getContext('2d');
 
+    
     context.beginPath();
     context.moveTo(100, 150);
     context.lineTo(350, 50);
     context.stroke(); 
-    console.log('ran')
+    canvas.style.height = '450'
+    
 }
 
 headerView();
